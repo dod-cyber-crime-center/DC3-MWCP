595314b027d2c1bd54eb193d3f7a171cf69bdbd0
finleyh <finley.d.hammond@outlook.com>
2025-01-27 11:09:42 -0500

    python is a constrictor by nature, whitespace inconsistency fix

--- mwcp/parsers/CookieFinder.py
+++ mwcp/parsers/CookieFinder.py
@@ -15,8 +15,7 @@ class GenericVOne(Parser):
         return True
 
     def run(self):
-        #regex=r'url\s*[^\s]\s*([^\n]*)\n\s*username\s*[^\s]\s*([^\n]*)\n\s*password\s*[^\s]\s*([^\n]*)[\n$\s]'
-	regex = r'(^\s+)\x09(?:TRUE|FALSE)\x09\x2F\x09(?:TRUE|FALSE)\x09([123]\d{9})\x09([^\n$]+)'
+        regex = r'(^\s+)\x09(?:TRUE|FALSE)\x09\x2F\x09(?:TRUE|FALSE)\x09([123]\d{9})\x09([^\n$]+)'
         logger.info(f"{self.DESCRIPTION} by {self.AUTHOR}")
         file_content = self.file_object.data.decode(errors="backslashreplace")
         matches = re.findall(regex, file_content, re.IGNORECASE)
