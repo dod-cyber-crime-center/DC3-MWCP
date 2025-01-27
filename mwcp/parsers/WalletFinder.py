3c4f8c9a43f3960e2be276b5af9a1cec27416c02
finleyh <finley.d.hammond@outlook.com>
2025-01-27 16:40:47 -0500

    added autofill parser

---
+++ mwcp/parsers/WalletFinder.py
@@ -0,0 +1,23 @@
+import re
+import logging
+from mwcp import Parser, metadata
+import pdb
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+class GenericVOne(Parser):
+    DESCRIPTION = "Genericv1 parser of cookie files, matches Chromium based cookie theft (MS/Google)"
+    AUTHOR = "fh"
+
+    @classmethod
+    def identify(cls, file_object):
+        return True
+
+    def run(self):
+        regex = r'([a-zA-Z0-9-\._]+)\x09(?:TRUE|FALSE)\x09\x2F\x09(?:TRUE|FALSE)\x09([123]\d{9})\x09([^\n]+)'
+        logger.info(f"{self.DESCRIPTION} by {self.AUTHOR}")
+        file_content = self.file_object.data.decode(errors="backslashreplace")
+        matches = re.findall(regex, file_content, re.IGNORECASE)
+        for m in matches:
+            self.report.add(metadata.Credential(m))
---
+++ mwcp/yara_repo/AutofillFinder.yar
@@ -0,0 +1,20 @@
+rule GenericVOneRule
+{
+	meta:
+		description = "Generic Yara rule to detect Autofill data"
+		author = "FH"
+		date = "2025-01-27"
+		version = "1.0"
+		mwcp = "AutofillFinder.GenericVOne"
+
+	strings:
+		$firstname="firstname" ascii wide nocase
+		$lastname="lastname" ascii wide nocase
+		$applica="applica" ascii wide nocase
+		$email = "email" ascii wide nocase
+		$address = "address" ascii wide nocase
+		$special1 = ":" ascii wide
+
+	condition:
+		$firstname and $lastname and ($applica or $email or $address) and #special1>10
+}
